f0b6d395439556a59033086168e427b4b99237ac
William Pringle <wpringle@nd.edu>
2023-04-24 19:54:27 +0000

    reformatting GSA, adding ability to do use GradientBooster that does a different surrogate model for each eigenfeature

--- workflow/surrogate/gsa.py
+++ workflow/surrogate/gsa.py
@@ -1,5 +1,6 @@
 import numpy as np
 from wrf_fvcom.variables import PerturbedVariable, VariableDistribution
+from surrogate.tree_regression import surrogate_model_predict
 
 ###########################################
 #  ____     ___    ____     ___    _      #
@@ -12,16 +13,16 @@ from wrf_fvcom.variables import PerturbedVariable, VariableDistribution
 
 
 class smethod(object):
-    def __init__(self,variable_matrix,sens_names):
-        self.dim=variable_matrix.shape[1]
+    def __init__(self, variable_matrix, sens_names):
+        self.dim = variable_matrix.shape[1]
         self.sens = dict((k, [None] * self.dim) for k in self.sens_names)
         self.sens_ready = dict((k, [False] * self.dim) for k in self.sens_names)
-        self.ptypes = [] 
+        self.ptypes = []
         for scheme in variable_matrix['scheme']:
             variable = PerturbedVariable.class_from_scheme_name(scheme)
             if variable.variable_distribution == VariableDistribution.DISCRETEUNIFORM:
                 self.ptypes.append('int')
-            else: 
+            else:
                 self.ptypes.append('float')
 
 
@@ -29,83 +30,88 @@ class sobol(smethod):
     ## Main and total are based on Saltelli 2010; it computes joint in the total sense!
     ##
     ## Initialization
-    def __init__(self,variable_matrix):
-        print("Initializing SOBOL")
-        self.sens_names=['main', 'total', 'jointt']
-        smethod.__init__(self,variable_matrix,self.sens_names)
+    def __init__(self, variable_matrix):
+        print('Initializing SOBOL')
+        self.sens_names = ['main', 'total', 'jointt']
+        smethod.__init__(self, variable_matrix, self.sens_names)
 
-    def sample(self,ninit,parameter_types=None):
-        print("Sampling SOBOL")
+    def sample(self, ninit, parameter_types=None):
+        print('Sampling SOBOL')
 
-        sam1 = np.random.rand(ninit,self.dim)
-        sam2 = np.random.rand(ninit,self.dim)
- 
+        sam1 = np.random.rand(ninit, self.dim)
+        sam2 = np.random.rand(ninit, self.dim)
+
         for pp, par_type in enumerate(self.ptypes):
             if par_type == 'int':
-                sam1[:,pp] = np.round(sam1[:,pp]) 
-                sam2[:,pp] = np.round(sam2[:,pp])
+                sam1[:, pp] = np.round(sam1[:, pp])
+                sam2[:, pp] = np.round(sam2[:, pp])
 
-        xsam=np.vstack((sam1,sam2))
+        xsam = np.vstack((sam1, sam2))
 
         for id in range(self.dim):
-            samid=sam1.copy()
-            samid[:,id]=sam2[:,id]
-            xsam=np.vstack((xsam,samid))
+            samid = sam1.copy()
+            samid[:, id] = sam2[:, id]
+            xsam = np.vstack((xsam, samid))
 
-        self.nsam=xsam.shape[0]
-        self.sens_ready['main']   = True
-        self.sens_ready['total']  = True
+        self.nsam = xsam.shape[0]
+        self.sens_ready['main'] = True
+        self.sens_ready['total'] = True
         self.sens_ready['jointt'] = True
 
         return xsam
 
-    def compute(self,ysam,computepar=None):
-        ninit=self.nsam//(self.dim+2)
-        y1=ysam[ninit:2*ninit]
-        var=np.var(ysam[:2*ninit])
-        si=np.zeros((self.dim,))
-        ti=np.zeros((self.dim,))
-        jtij=np.zeros((self.dim,self.dim))
+    def compute(self, ysam, computepar=None):
+        ninit = self.nsam // (self.dim + 2)
+        y1 = ysam[ninit : 2 * ninit]
+        var = np.var(ysam[: 2 * ninit])
+        si = np.zeros((self.dim,))
+        ti = np.zeros((self.dim,))
+        jtij = np.zeros((self.dim, self.dim))
 
         for id in range(self.dim):
-            y2=ysam[2*ninit+id*ninit:2*ninit+(id+1)*ninit]-ysam[:ninit]
-            si[id]=np.mean(y1*y2)/var
-            ti[id]=0.5*np.mean(y2*y2)/var
+            y2 = ysam[2 * ninit + id * ninit : 2 * ninit + (id + 1) * ninit] - ysam[:ninit]
+            si[id] = np.mean(y1 * y2) / var
+            ti[id] = 0.5 * np.mean(y2 * y2) / var
             for jd in range(id):
-                y3=ysam[2*ninit+id*ninit:2*ninit+(id+1)*ninit]-ysam[2*ninit+jd*ninit:2*ninit+(jd+1)*ninit]
-                jtij[id,jd]=ti[id]+ti[jd]-0.5*np.mean(y3*y3)/var
+                y3 = (
+                    ysam[2 * ninit + id * ninit : 2 * ninit + (id + 1) * ninit]
+                    - ysam[2 * ninit + jd * ninit : 2 * ninit + (jd + 1) * ninit]
+                )
+                jtij[id, jd] = ti[id] + ti[jd] - 0.5 * np.mean(y3 * y3) / var
 
-        self.sens['main']=si
-        self.sens['total']=ti
-        self.sens['jointt']=jtij.T
+        self.sens['main'] = si
+        self.sens['total'] = ti
+        self.sens['jointt'] = jtij.T
 
         return self.sens
 
-def compute_sensitivities(surrogate_model, variable_matrix, sample_size = 10000):
 
-    # get the sensitivity sample matrix 
+def compute_sensitivities(surrogate_model, variable_matrix, sample_size=10000):
+
+    # get the sensitivity sample matrix
     SensMethod = sobol(variable_matrix)
     xsam = SensMethod.sample(sample_size)
     # evaluate the surrogate model at the samples
-    ysam = surrogate_model.predict(xsam)
+    ysam = surrogate_model_predict(surrogate_model, xsam)
 
     npts = ysam.shape[1]
     variable_names = []
-    variable_prior = '' 
+    variable_prior = ''
     for sdx, scheme in enumerate(variable_matrix['scheme']):
         variable_name = PerturbedVariable.class_from_scheme_name(scheme).name
         if variable_name != variable_prior:
             variable_names.append(variable_name)
         variable_prior = variable_name
     ndim = len(variable_names)
-    sens_dict = { 'main': np.zeros((npts, ndim)), 
-                  'total': np.zeros((npts, ndim)),
-                  'variable_names': variable_names, 
+    sens_dict = {
+        'main': np.zeros((npts, ndim)),
+        'total': np.zeros((npts, ndim)),
+        'variable_names': variable_names,
     }
     for i in range(npts):
         sens = SensMethod.compute(ysam[:, i])
         vdx = -1
-        variable_prior = '' 
+        variable_prior = ''
         for sdx, scheme in enumerate(variable_matrix['scheme']):
             variable_name = PerturbedVariable.class_from_scheme_name(scheme).name
             if variable_name != variable_prior:
--- workflow/surrogate/tree_regression.py
+++ workflow/surrogate/tree_regression.py
@@ -6,41 +6,78 @@ from sklearn.model_selection import cross_val_score, LeaveOneOut, ShuffleSplit, 
 
 def kl_scorer(eigenratio):
     def weighted_rmse(y_true, y_pred):
-        wmse = ((y_true - y_pred) ** 2 * eigenratio).sum(axis=1)
+        if len(y_true.shape) == 2:
+            naxis = 1
+        else:
+            naxis = 0
+        wmse = ((y_true - y_pred) ** 2 * eigenratio).sum(axis=naxis)
         return np.mean(np.sqrt(wmse))
 
     return make_scorer(weighted_rmse, greater_is_better=False)
 
 
 def make_surrogate_model(
-    train_X, train_Y, eigenratio=1.0, regressor: str = 'RF', criterion: str = 'squared_error',
+    train_X,
+    train_Y,
+    eigenratio=1.0,
+    regressor: str = 'RF',
+    criterion: str = 'squared_error',
+    loss: str = 'squared_error',
 ):
 
-    if regressor == 'RF':
-        reg = RandomForestRegressor(random_state=666, criterion=criterion)
-    elif regressor == 'GB':
-        reg = GradientBoostingRegressor(random_state=666)
-        # GB_KL_model = []
-        # for iout in range(neig):
-        #    reg = GradientBoostingRegressor(n_estimators=n_estimators,max_features=0.3,random_state=666)
-        #    reg.fit(variable_matrix[1::], kl_xi[1::,iout])
-        #    GB_KL_model.append(reg)
-    else:
-        raise ValueError(f'{regressor} not recognized')
-
     nens, ndim = train_X.shape
+    nens_, neig = train_Y.shape
+    assert nens == nens_
     param_grid = {
         'n_estimators': [ndim, ndim * 2, ndim * 4],
         'max_features': [0.3, 0.6, 0.8, 1.0],
     }
     cv = LeaveOneOut()
-    scorer = kl_scorer(eigenratio)
-    grid_reg = GridSearchCV(reg, param_grid, cv=cv, scoring=scorer)
-    grid_reg.fit(train_X, train_Y)
 
-    surrogate_model = grid_reg.best_estimator_
-    surrogate_model.fit(train_X, train_Y)
+    if regressor == 'RF':
+        reg = RandomForestRegressor(random_state=666, criterion=criterion)
+        scorer = kl_scorer(eigenratio)
 
-    cv_score = cross_val_score(surrogate_model, train_X, train_Y, cv=cv, scoring=scorer)
+        grid_reg = GridSearchCV(reg, param_grid, cv=cv, scoring=scorer)
+        grid_reg.fit(train_X, train_Y)
+        best_params = grid_reg.best_params_
 
-    return surrogate_model, grid_reg.best_params_, cv_score
+        surrogate_model = grid_reg.best_estimator_
+        surrogate_model.fit(train_X, train_Y)
+
+        cv_score = cross_val_score(surrogate_model, train_X, train_Y, cv=cv, scoring=scorer)
+
+    elif regressor == 'GB':
+        reg = GradientBoostingRegressor(random_state=666, criterion=criterion, loss=loss)
+        scorer = kl_scorer(1.0)
+
+        surrogate_model = []
+        best_params = []
+        cv_score = []
+        for iout in range(neig):
+            train_yy = train_Y[:, iout]
+            grid_reg = GridSearchCV(reg, param_grid, cv=cv, scoring=scorer)
+            grid_reg.fit(train_X, train_yy)
+            best_params.append(grid_reg.best_params_)
+
+            sm = grid_reg.best_estimator_
+            sm.fit(train_X, train_yy)
+
+            cv_score.append(cross_val_score(sm, train_X, train_yy, cv=cv, scoring=scorer))
+
+            surrogate_model.append(sm)
+    else:
+        raise ValueError(f'{regressor} not recognized')
+
+    return surrogate_model, best_params, cv_score
+
+
+def surrogate_model_predict(surrogate_model, X_values):
+    if type(surrogate_model) == list:
+        Y_values = np.empty((X_values.shape[0], len(surrogate_model)))
+        for sdx, sm in enumerate(surrogate_model):
+            Y_values[:, sdx] = sm.predict(X_values)
+    else:
+        Y_values = surrogate_model.predict(X_values)
+
+    return Y_values
