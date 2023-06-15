dccc3e0b2cc8d11ab03edda51af9aeb450886ecc
William Pringle <wpringle@nd.edu>
2023-06-15 14:51:57 +0000

    adding ability to treat pyapprox PolynomialChaos surrogate

--- workflow/surrogate/utils.py
+++ workflow/surrogate/utils.py
@@ -1,5 +1,6 @@
 from numpoly import ndpoly
 from numpy import empty
+from pyapprox.surrogates.polychaos.gpc import PolynomialChaosExpansion
 
 def surrogate_model_predict(surrogate_model, X_values):
     if type(surrogate_model) is ndpoly:
@@ -8,6 +9,8 @@ def surrogate_model_predict(surrogate_model, X_values):
         Y_values = empty((X_values.shape[0], len(surrogate_model)))
         for sdx, sm in enumerate(surrogate_model):
             Y_values[:, sdx] = sm.predict(X_values)
+    elif type(surrogate_model) is PolynomialChaosExpansion:
+        Y_values = surrogate_model.value(X_values.T)
     else:
         Y_values = surrogate_model.predict(X_values)
 
