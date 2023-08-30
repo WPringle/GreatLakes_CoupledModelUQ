48a4b6c83bc0d3b9a0e4ddec451be736d681fe28
William Pringle <wpringle@nd.edu>
2023-08-30 21:16:09 +0000

    change list surrogate model to be about getting the average prediction

--- workflow/surrogate/utils.py
+++ workflow/surrogate/utils.py
@@ -1,13 +1,21 @@
 from numpoly import ndpoly
-from numpy import empty
+
+# from numpy import empty
+
 
 def surrogate_model_predict(surrogate_model, X_values):
     if type(surrogate_model) is ndpoly:
         Y_values = surrogate_model(*X_values.T).T
     elif type(surrogate_model) == list:
-        Y_values = empty((X_values.shape[0], len(surrogate_model)))
+        # Y_values = empty((X_values.shape[0], len(surrogate_model)))
+        n_folds = len(surrogate_model)
         for sdx, sm in enumerate(surrogate_model):
-            Y_values[:, sdx] = sm.predict(X_values)
+            if sdx == 0:
+                Y_values = sm.predict(X_values)
+            else:
+                Y_values += sm.predict(X_values)
+            # Y_values[:, sdx] = sm.predict(X_values)
+        Y_values /= n_folds
     else:
         Y_values = surrogate_model.predict(X_values)
 
